1a6f1c7786e4a2c32f544a8f16093b94f9100e6a
MeirOverferst <meiroverferst@gmail.com>
2019-08-16 11:01:47 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,87 @@
-import React from 'react';
-import logo from './logo.svg';
+import algoliasearch from 'algoliasearch/lite';
+import React, { Component } from 'react';
+import {InstantSearch,Hits,Pagination,Highlight,Configure,connectSearchBox  } from 'react-instantsearch-dom';
+import PropTypes from 'prop-types';
 import './App.css';
 
-function App() {
+const client = algoliasearch(
+  'latency',
+  '6be0576ff61c053d5f9a3225e2a90f76'
+);
+
+class SearchBox extends Component {
+  timerId = null;
+
+  state = {
+    value: this.props.currentRefinement
+  };
+
+  onChangeDebounced = event => {
+    const { refine, delay } = this.props;
+    const value = event.currentTarget.value;
+    clearTimeout(this.timerId);
+    this.timerId = setTimeout(() => refine(value), delay);
+    this.setState(() => ({
+      value
+    }));
+  };
+  render() {
+    const value = this.state.value;
+    return (
+      <div className="searchbar"> 
+      <input 
+        value={value}
+        onChange={this.onChangeDebounced}
+        placeholder="Search here..."
+      />
+      </div>
+    );
+  }
+}
+
+const DebouncedSearchBox = connectSearchBox(SearchBox);
+
+class App extends Component {
+  render() {
+    return (
+      <div className="ais-InstantSearch">
+       
+      <InstantSearch  indexName="ikea" searchClient={client}>
+    
+          <div className="left-panel">
+            <Configure hitsPerPage={18} />
+          </div>
+          <div className="right-panel">
+          <DebouncedSearchBox delay={200} />
+            <Hits hitComponent={Hit} />
+            <Pagination />
+          </div>
+        </InstantSearch>
+      </div>
+    );
+  }
+}
+
+function Hit(props) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <img src={props.hit.image} className="image"  />
+      <div >
+        <Highlight attribute="name" hit={props.hit} />
+      </div>
+      <div className="hit-description">
+        <Highlight attribute="description" hit={props.hit} />
+      </div>
+      <div className="hit-price">${props.hit.price}</div>
     </div>
   );
 }
 
+Hit.propTypes = {
+  hit: PropTypes.object.isRequired,
+};
+
 export default App;
+
+
+
